778af8de233a7a1200a1ca0684d7b228a95a195e
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-08-24 13:15:00 +0200

    Remove all superfluous deps and use workspace protocolo

--- packages/spor-theme-react/src/index.ts
+++ packages/spor-theme-react/src/index.ts
@@ -1,14 +1,15 @@
 import { theme as defaultTheme } from "@chakra-ui/theme";
+import { createTheme } from "@chakra-ui/theme-tools";
 import * as components from "./components";
 import * as foundations from "./foundations";
 
-export const theme = {
+export const theme = createTheme({
   ...defaultTheme,
   ...foundations,
   components: {
     ...defaultTheme.components,
     ...components,
   },
-};
+});
 
 export { fontFaces } from "./font-faces";
